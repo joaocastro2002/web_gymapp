e5b370273fd98b299f0f721ae96af0089a2ca1b9
João Castro <joaocastro2002@gmail.com>
2022-05-23 22:16:31 +0100

    .

--- src/app/aluno/comparar-avaliacao-fisica/dados-comparar-avaliacao/dados-comparar-avaliacao.component.ts
+++ src/app/aluno/comparar-avaliacao-fisica/dados-comparar-avaliacao/dados-comparar-avaliacao.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DadosCompararAvaliacaoComponent implements OnInit {
 
+  dados = []
+
   constructor() { }
 
   ngOnInit(): void {
+    this.dados = [{nome: 'Peso', valor: '85 Kg'},
+    {nome: 'Massa Muscular', valor: '35 Kg'},
+    {nome: 'Gordura Corporal', valor: '30%'},
+    {nome: 'Gordura Visceral', valor: '10%'},
+    {nome: 'Água', valor: '73%'},
+    {nome: 'Proteína', valor: '50%'},
+    {nome: 'Massa Óssea', valor: '10 Kg'},
+    {nome: 'Metabolismo Basal', valor: '50 Kcal'}]
   }
 
 }
