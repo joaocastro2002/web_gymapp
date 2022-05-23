449ba87fb8e13981ee05d31abbc2644e3178472d
“EngenheiroHenrique” <a21122@alunos.ipca.pt>
2022-05-23 11:33:36 +0100

    .criar medidas

--- src/app/treinador/criar-avaliacao-fisica/criar-medidas/criar-medidas.component.ts
+++ src/app/treinador/criar-avaliacao-fisica/criar-medidas/criar-medidas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-medidas',
@@ -8,8 +9,64 @@ import { Component, OnInit } from '@angular/core';
 export class CriarMedidasComponent implements OnInit {
 
   constructor() { }
-
+  medidas = []
+  medidasFormControl = {}
+  avaliacaoForm: FormGroup
   ngOnInit(): void {
+    this.getMedidasMarca()
   }
+  onSubmit() { }
 
+  getMedidasMarca() {
+    this.medidas = [
+      {
+        unilado: true,
+        nome: "Peito"
+      },
+      {
+        unilado: true,
+        nome: "Cintura"
+      },
+      {
+        unilado: true,
+        nome: "Abdómen"
+      },
+      {
+        unilado: true,
+        nome: "Quadril"
+      },
+      {
+        unilado: false,
+        nome: "Tricep"
+      },
+      {
+        unilado: false,
+        nome: "Bicep"
+      },
+      {
+        unilado: false,
+        nome: "Coxa"
+      },
+      {
+        unilado: false,
+        nome: "Gémeo inferior"
+      },
+
+    ]
+    for (let medida of this.medidas) {
+      this.medidasFormControl[medida.nome] = new FormControl(null, Validators.required)
+
+
+    }
+    this.medidasFormControl['peso'] = new FormControl(null, Validators.required)
+    this.medidasFormControl['Musculo'] = new FormControl(null, Validators.required)
+    this.medidasFormControl['Água'] = new FormControl(null, Validators.required)
+    this.medidasFormControl['Proteina'] = new FormControl(null, Validators.required)
+    this.medidasFormControl['Gordura Corporal'] = new FormControl(null, Validators.required)
+    this.medidasFormControl['Gordura Visceral'] = new FormControl(null, Validators.required)
+    this.medidasFormControl['Massa Ossea'] = new FormControl(null, Validators.required)
+    this.medidasFormControl['Metabolismo Basal'] = new FormControl(null, Validators.required)
+    this.medidasFormControl['IMC'] = new FormControl(null, Validators.required)
+    this.avaliacaoForm = new FormGroup(this.medidasFormControl)
+  }
 }
